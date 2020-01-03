f9135bee86eeaa57cc57f4faacec9d4170f0f121
Omarhd <omeralhadu@gmail.com>
2020-01-03 14:43:34 +0200

    Initail commit

---
+++ GoGo Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoGo Admin
+//
+//  Created by Omar on 03/01/2020.
+//  Copyright © 2020 Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
